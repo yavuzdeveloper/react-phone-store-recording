906736f826469c1dc024824a7d149cd8680de5d2
Yavuz Tokus <yavuztokus2010@gmail.com>
2020-12-26 17:42:45 +0300

    first commit my big project created

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
+import { ProductProvider } from "./context";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ProductProvider>
+    <Router>
+        <App />
+      </Router>
+  </ProductProvider>,
   document.getElementById('root')
 );
 
